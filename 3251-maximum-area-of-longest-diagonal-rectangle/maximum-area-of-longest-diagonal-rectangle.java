58f83081fc00e4c55545ecfd8bdeeeb0d9e3c860
Aman Singh <amansingh102006@gmail.com>
2026-05-02 00:36:07 +0530

    Time: 1 ms (99.69%) | Memory: 46.6 MB (29.06%) - LeetSync

--- 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.java
+++ 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.java
@@ -1,19 +1,16 @@
 class Solution {
     public int areaOfMaxDiagonal(int[][] arr) {
-        int max = arr[0][0] * arr[0][1];
-        int m = arr.length;
-
-        double max1 = Math.sqrt((arr[0][0]*arr[0][0]) + (arr[0][1]*arr[0][1]));
-
-        for(int i = 0; i < m; i++)
+        int max=arr[0][0]*arr[0][1];
+        double max1=Math.sqrt((arr[0][0]*arr[0][0])+(arr[0][1]*arr[0][1]));
+        int n=arr.length;
+        for(int i=0;i<n;i++)
         {
-            int temp = (arr[i][0]*arr[i][0]) + (arr[i][1]*arr[i][1]);
-            double sqr = Math.sqrt(temp);
-
-            if (max1 < sqr || (max1 == sqr && max < arr[i][0] * arr[i][1]))
+            int temp=(arr[i][0]*arr[i][0])+(arr[i][1]*arr[i][1]);
+            double sqrt=Math.sqrt(temp);
+            if(max1<sqrt || (max1==sqrt && max<arr[i][0]*arr[i][1]))
             {
-                max = arr[i][0] * arr[i][1];
-                max1 = sqr;
+                max=arr[i][0]*arr[i][1];
+                max1=sqrt;
             }
         }
         return max;
